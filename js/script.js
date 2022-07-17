c037cf57246f9f4e043451881b106cc8bc25807a
Ali Bahadoran <alibahadoran.eng@gmail.com>
2022-07-17 22:13:05 +0430

    End Project

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function applyTheme(theme) {
+    document.body.classList.remove("theme-auto", "theme-light", "theme-dark");
+    document.body.classList.add(`theme-${theme}`);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const saveTheme = localStorage.getItem("theme") || "auto";
+
+    applyTheme(saveTheme);
+
+    for (const opationElement of document.querySelectorAll("#selTheme option")) {
+        opationElement.selected = saveTheme === opationElement.value;
+    }
+
+    document.querySelector("#selTheme").addEventListener("change", function () {
+        localStorage.setItem("theme", this.value);
+        applyTheme(this.value);
+    });
+});
